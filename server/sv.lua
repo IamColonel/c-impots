cbd1c548b3c85961ab3bcd72ba818c6b440309ad
dedoxtv <unmecpasfort@gmail.com>
2022-06-19 16:16:46 +0200

    Create server thread

--- server/sv.lua
+++ server/sv.lua
@@ -1,44 +1,9 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('C_Impots:chomage')
-AddEventHandler('C_Impots:chomage', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerEvent('esx_addonaccount:getSharedAccount', Config.Society_gouv, function(account)
-        societyAccount = account
-    end)
-
-    xPlayer.removeAccountMoney('bank', Config.Impots.Chomage)
-    if Config.Gouv == true then
-        societyAccount.addMoney(Config.Impots.Chomage)
+CreateThread(function()
+    while true do
+        Wait(Config.Time)
+        TriggerClientEvent("C_Impots:checkPlayerJob", -1)
     end
-    TriggerClientEvent('esx:showAdvancedNotification', source, 'Banque Centrale', 'Impôts', 'Vous avez été prélevé de~g~ '..Config.Impots.Chomage..'$~s~ sur votre compte bancaire !', 'CHAR_BANK_FLEECA', 2)
-end)
-
-RegisterServerEvent('C_Impots:public')
-AddEventHandler('C_Impots:public', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerEvent('esx_addonaccount:getSharedAccount', Config.Society_gouv, function(account)
-        societyAccount = account
-    end)
-
-    xPlayer.removeAccountMoney('bank', Config.Impots.Public)
-    if Config.Gouv == true then
-        societyAccount.addMoney(Config.Impots.Public)
-    end
-    TriggerClientEvent('esx:showAdvancedNotification', source, 'Banque Centrale', 'Impôts', 'Vous avez été prélevé de~g~ '..Config.Impots.Public..'$~s~ sur votre compte bancaire !', 'CHAR_BANK_FLEECA', 2)
-end)
-
-RegisterServerEvent('C_Impots:prive')
-AddEventHandler('C_Impots:prive', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerEvent('esx_addonaccount:getSharedAccount', Config.Society_gouv, function(account)
-        societyAccount = account
-    end)
-
-    xPlayer.removeAccountMoney('bank', Config.Impots.Prive)
-    if Config.Gouv == true then
-        societyAccount.addMoney(Config.Impots.Prive)
-    end
-    TriggerClientEvent('esx:showAdvancedNotification', source, 'Banque Centrale', 'Impôts', 'Vous avez été prélevé de~g~ '..Config.Impots.Prive..'$~s~ sur votre compte bancaire !', 'CHAR_BANK_FLEECA', 2)
 end)
